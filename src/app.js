564af06f4afe224bd8f8c63ac8e4bea6a721cc84
Ezequiel Lima <joao.lima94@fatec.sp.gov.br>
2022-09-06 17:14:11 -0300

    Adicionado CRUD

--- src/app.js
+++ src/app.js
@@ -2,9 +2,16 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 const router = express.Router();
+
+// Connecta ao banco
+mongoose.connect('mongodb+srv://fatec_group:KULFvu1lRdCo3yCZ@nodestorage.sduiuix.mongodb.net');
+
+// Carregar os Models
+const Product = require('./models/product');
 
 // Carregar as Rotas
 const indexRoute = require('./routes/index-route');
--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -1,17 +1,93 @@
 'use strict';
 
+const { default: mongoose } = require("mongoose");
+const Product = mongoose.model('Product');
+
+exports.get = (req, res, next) => {
+    Product.find({ 
+        active: true // consulta somente os produtos ativos
+    }, 'title price slug') // campos que eu quero retornar
+    .then(data => {
+        res.status(200).send(data);
+    }).catch(erro => {
+        res.status(400).send(erro); 
+    });
+}
+
+exports.getBySlug = (req, res, next) => {
+    Product.findOne({ 
+        slug: req.params.slug, // slug que vai na rota
+        active: true // consulta somente os produtos ativos
+    }, 'title description price slug tags') // campos que eu quero retornar
+    .then(data => {
+        res.status(200).send(data);
+    }).catch(erro => {
+        res.status(400).send(erro); 
+    });
+}
+
+exports.getById = (req, res, next) => {
+    Product.findById(req.params.id) // retornar por id
+    .then(data => {
+        res.status(200).send(data);
+    }).catch(erro => {
+        res.status(400).send(erro); 
+    });
+}
+
+exports.getByTag = (req, res, next) => {
+    Product.find({
+        tags: req.params.tag, // consulta por tag
+        active: true
+    }, 'title description price slug tags') 
+    .then(data => {
+        res.status(200).send(data);
+    }).catch(erro => {
+        res.status(400).send(erro); 
+    });
+}
+
 exports.post = (req, res, next) => {
-    res.status(201).send(req.body);
+    var product = new Product(req.body);
+    product.save()
+        .then(x => {
+            res.status(201).send({ message: 'Produto cadastrado com sucesso'});
+        }).catch(e => {
+            res.status(400).send({ 
+                message: 'Falha ao cadastrar o produto', 
+                data: e}); 
+        });
 };
 
 exports.put = (req, res, next) => {
-    const id = req.params.id;
-    res.status(200).send({
-        id: id,
-        item: req.body
+    Product.findByIdAndUpdate(req.params.id, {
+        $set: {
+            title: req.body.title,
+            description: req.body.description,
+            price: req.body.price
+        }
+    }).then(x => {
+        res.status(201).send({
+            message: 'Produto atualizado com sucesso!'
+        });
+    }).catch(e => {
+        res.status(400).send({
+            message: 'Falha ao atualizar produto',
+            data: e
+        });
     });
 };
 
 exports.delete = (req, res, next) => {
-    res.status(200).send(req.body);
+    Product.findByIdAndRemove(req.params.id)
+    .then(x => {
+        res.status(200).send({
+            message: 'Produto removido com sucesso!'
+        });
+    }).catch(e => {
+        res.status(400).send({
+            message: 'Falha ao remover o produto',
+            data: e
+        });
+    });
 };
