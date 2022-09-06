deac0aad72bb64a1cd86210cc192889eebb7e450
Ezequiel Lima <joao.lima94@fatec.sp.gov.br>
2022-09-06 18:11:52 -0300

    Adicionado Validator

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -2,6 +2,7 @@
 
 const { default: mongoose } = require("mongoose");
 const Product = mongoose.model('Product');
+const ValidationContract = require('../validators/fluent-validator');
 
 exports.get = (req, res, next) => {
     Product.find({ 
@@ -48,11 +49,23 @@ exports.getByTag = (req, res, next) => {
 }
 
 exports.post = (req, res, next) => {
+    let contract = new ValidationContract();
+    contract.hasMinLen(req.body.title, 3, 'O titulo deve conter pelo menos 3 caracteres');
+    contract.hasMinLen(req.body.slug, 3, 'O slug deve conter pelo menos 3 caracteres');
+    contract.hasMinLen(req.body.description, 3, 'O descrição deve conter pelo menos 3 caracteres');
+    
+    // Se os dados forem inválidos
+    if (!contract.isValid()) {
+        res.status(400).send(contract.errors()).end();
+        return;
+    }
+
     var product = new Product(req.body);
     product.save()
         .then(x => {
             res.status(201).send({ message: 'Produto cadastrado com sucesso'});
-        }).catch(e => {
+        })
+        .catch(e => {
             res.status(400).send({ 
                 message: 'Falha ao cadastrar o produto', 
                 data: e}); 
