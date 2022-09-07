b7c92ceb17bca0e74aec1570536eadb6fabaff1a
Ezequiel Lima <joao.lima94@fatec.sp.gov.br>
2022-09-07 13:43:07 -0300

    Adicionado async e await

--- src/repositories/product-repository.js
+++ src/repositories/product-repository.js
@@ -3,52 +3,50 @@
 const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
 
-exports.get = () => {
-    return Product
-        .find({
-            active: true // consulta somente os produtos ativos
-        }, 'title price slug'); // campos que eu quero retornar
+exports.get = async () => {
+    const res = await Product.find({
+        active: true // consulta somente os produtos ativos
+    }, 'title price slug'); // campos que eu quero retornar
+    return res;
 }
 
-exports.getBySlug = (slug) => {
-    return Product
-        .findOne({
-            slug: slug, // slug que vai na rota
-            active: true // consulta somente os produtos ativos
-        }, 'title description price slug tags'); // campos que eu quero retornar
+exports.getBySlug = async (slug) => {
+    const res = await Product.findOne({
+        slug: slug, // slug que vai na rota
+        active: true // consulta somente os produtos ativos
+    }, 'title description price slug tags'); // campos que eu quero retornar
+    return res;
 }
 
-exports.getById = (id) => {
-    return Product
-        .findById(id); // retornar por id
+exports.getById = async (id) => {
+    const res = await Product.findById(id); // retornar por id
+    return res;
 }
 
-exports.getByTag = (tag) => {
-    return Product
-        .find({
-            tags: tag, // consulta por tag
-            active: true
-        }, 'title description price slug tags');
+exports.getByTag = async (tag) => {
+    const res = Product.find({
+        tags: tag, // consulta por tag
+        active: true
+    }, 'title description price slug tags');
+    return res;
 }
 
-exports.create = (data) => {
+exports.create = async (data) => {
     var product = new Product(data);
-    return product.save();
+    await product.save();
 }
 
-exports.update = (id, data) => {
-    return Product
-        .findByIdAndUpdate(id, {
-            $set: {
-                title: data.title,
-                description: data.description,
-                price: data.price,
-                slug: data.slug
-            }
-        });
+exports.update = async (id, data) => {
+    await Product.findByIdAndUpdate(id, {
+        $set: {
+            title: data.title,
+            description: data.description,
+            price: data.price,
+            slug: data.slug
+        }
+    });
 }
 
-exports.delete = (id) => {
-    return Product
-        .findByIdAndRemove(id);
+exports.delete = async (id) => {
+    await Product.findByIdAndRemove(id);
 }
