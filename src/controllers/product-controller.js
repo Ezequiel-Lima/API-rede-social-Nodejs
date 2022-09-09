b9c69c50473c7356dbb7ef44fd4c7657f91e0263
Ezequiel Lima <joao.lima94@fatec.sp.gov.br>
2022-09-09 17:45:41 -0300

    Adicionado Order e Customer

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const { default: mongoose } = require("mongoose");
-const Product = mongoose.model('Product');
 const ValidationContract = require('../validators/fluent-validator');
 const repository = require('../repositories/product-repository');
 
