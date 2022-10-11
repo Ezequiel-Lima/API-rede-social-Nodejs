8321a596480ea6ab422399a882b37b909a4afb29
Ezequiel Lima <joao.lima94@fatec.sp.gov.br>
2022-10-11 11:30:53 -0300

    melhorado cors

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const config = require('./config');
+const cors = require('cors');
 
 const app = express();
 const router = express.Router();
@@ -32,6 +33,7 @@ app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    app.use(cors());
     next();
 });
 
